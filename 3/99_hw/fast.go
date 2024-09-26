40d36c9f4196dc1411f724b5bb0a63185227652f
AnVladic <anvladic13@gmail.com>
2024-09-26 18:33:24 +0300

    complite: hw 3

--- 3/99_hw/fast.go
+++ 3/99_hw/fast.go
@@ -1,18 +1,105 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
 )
 
-// вам надо написать более быструю оптимальную этой функции
+// User EasyJet:json
+type User struct {
+	Browsers []string `json:"browsers"`
+	Email    string   `json:"email"`
+	Name     string   `json:"name"`
+}
+
+// FastSearch вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	/*
-		!!! !!! !!!
-		обратите внимание - в задании обязательно нужен отчет
-		делать его лучше в самом начале, когда вы видите уже узкие места, но еще не оптимизировалм их
-		так же обратите внимание на команду в параметром -http
-		перечитайте еще раз задание
-		!!! !!! !!!
-	*/
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	r := regexp.MustCompile("@")
+	var seenBrowsers []string
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	lines := strings.Split(string(fileContents), "\n")
+
+	users := make([]User, 0, len(lines))
+	for _, line := range lines {
+		user := User{}
+		// fmt.Printf("%v %v\n", err, line)
+		//err := json.Unmarshal([]byte(line), &user)
+		err := user.UnmarshalJSON([]byte(line))
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, user)
+	}
+
+	for i, user := range users {
+
+		isAndroid := false
+		isMSIE := false
+
+		browsers := user.Browsers
+
+		for _, browserRaw := range browsers {
+			browser := browserRaw
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		for _, browserRaw := range browsers {
+			browser := browserRaw
+
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android and MSIE user:", user["name"], user["email"])
+		email := r.ReplaceAllString(user.Email, " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
---
+++ 3/99_hw/report.md
@@ -0,0 +1,8 @@
+![img.png](data/img.png) 
+Самая большая нагрузка тут в MatchString
+потом тут 
+```markdown
+user := make(map[string]interface{})
+// fmt.Printf("%v %v\n", err, line)
+err := json.Unmarshal([]byte(line), &user)
+```
